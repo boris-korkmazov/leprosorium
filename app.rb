1c704c7e23f48303ed432e78136173dd98b7d119
boriz <boriskorkmazov1989@gmail.com>
2015-02-17 18:10:40 +0300

    add new_post.erb

--- app.rb
+++ app.rb
@@ -5,3 +5,8 @@ require 'sinatra/reloader'
 get '/' do
 	erb "Hello! <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified for <a href=\"http://rubyschool.us/\">Ruby School</a>"			
 end
+
+
+get '/new_post' do
+  erb :new_post
+end
