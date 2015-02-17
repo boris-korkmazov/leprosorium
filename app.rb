a8e442f1ada3360be17efa45ad0aaba3eaef6848
boriz <boriskorkmazov1989@gmail.com>
2015-02-17 21:07:38 +0300

    Read content variable from post

--- app.rb
+++ app.rb
@@ -2,6 +2,8 @@ require 'rubygems'
 require 'sinatra'
 require 'sinatra/reloader'
 
+
+
 get '/' do
 	erb "Hello! <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified for <a href=\"http://rubyschool.us/\">Ruby School</a>"			
 end
@@ -9,4 +11,9 @@ end
 
 get '/new_post' do
   erb :new_post
+end
+
+post '/new_post' do
+  content = params[:content]
+  erb "You typed '#{content}'"
 end
